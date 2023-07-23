1d7ecdfa2a1a68a18156ba0d9b0f189a223352dc
Jakubpatelski <jakubpatelski24@gmail.com>
2023-07-23 20:42:33 +0200

    Initial Commit

---
+++ ShapeBreed/ShapeBreedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeBreedApp.swift
+//  ShapeBreed
+//
+//  Created by Jakub Patelski on 23/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShapeBreedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
